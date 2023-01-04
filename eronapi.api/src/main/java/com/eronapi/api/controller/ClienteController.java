effd2bb774f9bac1d9fa20f80883069618d93ce7
Eron Moraes <eron.moraes7@gmail.com>
2023-01-04 18:05:10 -0300

    commit final

--- eronapi.api/src/main/java/com/eronapi/api/controller/ClienteController.java
+++ eronapi.api/src/main/java/com/eronapi/api/controller/ClienteController.java
@@ -3,8 +3,12 @@ package com.eronapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eronapi.api.model.Cliente;
@@ -21,6 +25,12 @@ public class ClienteController {
 	public List<Cliente>listar(){
 		return clienteRepository.findAll();
 		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente add(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
