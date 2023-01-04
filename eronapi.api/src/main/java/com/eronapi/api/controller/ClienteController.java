dee869b7628f3de3433733a650c87dee119cd4a1
Eron Moraes <eron.moraes7@gmail.com>
2023-01-04 16:06:21 -0300

    first commit

---
+++ eronapi.api/src/main/java/com/eronapi/api/controller/ClienteController.java
@@ -0,0 +1,16 @@
+package com.eronapi.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping
+	public String hello(){
+		return "Olá mundo!";
+	}
+
+}
